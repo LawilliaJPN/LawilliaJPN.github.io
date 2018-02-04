dcc300f5105de6798d6a7e9cdcb0107f4added32
LawilliaNote <lawillia.jp@gmail.com>
2018-02-04 17:51:22 +0900

    2018-02-04 [Add} 和音再生：三和音実装

--- wapp/chord/chord.js
+++ wapp/chord/chord.js
@@ -3,17 +3,18 @@ var root = 0;
 var chord = [0, 4, 7];
 var tempRoot = 0;
 var tempChord = [0, 4, 7];
+var chordId = 0;
 
 /* 再生・停止 */
 function start() {
     play = true;
-    outputMsg("btnPlay", "停止");
+    updateHtml("btnPlay", "停止");
     startSounds();
 }
 
 function stop() {
     play = false;
-    outputMsg("btnPlay", "再生");
+    updateHtml("btnPlay", "再生");
     stopSounds()
 }
 
@@ -28,6 +29,7 @@ function updateSounds() {
     sound[BASS_SOUND][root].start();
     for (var i = 0; i < chord.length; i++) {
         sound[CHORD_SOUNDS][calcSound(root, chord[i])].start();
+        console.log(hz[CHORD_SOUNDS][calcSound(root, chord[i])]);
     }
 }
 
@@ -51,26 +53,69 @@ function calcSound(root, interval) {
 }
 
 /* 和音の変更 */
+function inputChord(id, num) {
+    resetButtonColor(getButtonId());
+    chordId = id;
+    setRoot(num);
+    setChord();
+    updateSounds();
+    setButtonColor(getButtonId());
+}
+
 function setRoot(num) {
     tempRoot = root;
     root = num;
-    updateSounds();
+}
+
+function setChord() {
+    tempChord = chord;
+
+    switch (chordId) {
+    case 0:
+        chord = [0, 4, 7];
+        break;
+    case 1:
+        chord = [0, 3, 7];
+        break;
+    case 2:
+        chord = [0, 3, 6];
+        break;
+    case 3:
+        chord = [0, 4, 8];
+        break;
+    }
 }
 
 /* 色の変更 */
 function setButtonColor(button) {
-	button.style.color = "white";
-	button.style.backgroundColor = "teal";
+	document.getElementById(button).style.color = "white";
+	document.getElementById(button).style.backgroundColor = "teal";
 }
 
 function resetButtonColor(button) {
-	button.style.color = "buttontext";
-	button.style.backgroundColor = "buttonface";
+	document.getElementById(button).style.color = "buttontext";
+	document.getElementById(button).style.backgroundColor = "buttonface";
 }
 
-/* 基本処理 */
-function outputMsg(id, str) {
-    document.getElementById(id).innerHTML = str;
+function getButtonId() {
+    typeOfButton = "btn";
+
+    switch (chordId) {
+    case 0:
+        typeOfButton += "Maj" +root;
+        break;
+    case 1:
+        typeOfButton += "Min" +root;
+        break;
+    case 2:
+        typeOfButton += "Dim" +root;
+        break;
+    case 3:
+        typeOfButton += "Aug" +root;
+        break;
+    }
+
+    return typeOfButton;
 }
 
 /* ボタン */
@@ -79,6 +124,18 @@ function btnPlay() {
     else start();
 }
 
-function btnRoot(num) {
-    setRoot(num);
+function btnMaj(num) {
+    inputChord(0, num);
+}
+
+function btnMin(num) {
+    inputChord(1, num);
+}
+
+function btnDim(num) {
+    inputChord(2, num);
+}
+
+function btnAug(num) {
+    inputChord(3, num);
 }
