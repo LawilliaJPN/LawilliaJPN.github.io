f85fc12b9988c9401be1622c4d0959595807acf3
LawilliaNote <lawillia.jp@gmail.com>
2018-04-28 13:25:39 +0900

    [add}神経衰弱：白黒モードの実装

--- contents/minigame/concentration/concentration.js
+++ contents/minigame/concentration/concentration.js
@@ -1,5 +1,5 @@
 var canvas, ctx;
-var game = {width:0, height:0, isRunning:false, timer:0, count:0, missCount:0, pair:0};
+var game = {width:0, height:0, mode:0, isRunning:false, timer:0, count:0, missCount:0, pair:0};
 // カード描画関連
 var card = {width:0, height:0, widthS:0, heightS:0, numX:0, numY:0};
 // 各カードの中身
@@ -31,7 +31,9 @@ window.onresize = function() {
 }
 
 /* ボタン */
-function btnStart() {
+function btnStart(mode) {
+    game.mode = mode;
+    setButtonColors();
     start();
 }
 
@@ -51,8 +53,10 @@ function concentration() {
 function start() {
     updateCanvas();
 
-    cards = ['0', '0', '1', '1', '2', '2', '3', '3', '4', '4',
-             '5', '5', '6', '6', '7', '7', '8', '8', '9', '9'];
+    cards.length = 0;
+    for (var i = 0; i < 20; i++) {
+        cards[i] = i % 10;
+    }
     shuffleCards(cards, 100);
 
     isPair = [false, false, false, false, false, false, false, false, false, false,
@@ -173,12 +177,21 @@ function drawGame() {
     for (var y = 0; y < card.numY; y++){
         for (var x = 0; x < card.numX; x++){
             if (!isPair[i]) {
+                if ((i == front.first) || (i == front.second)) {
+                    switch (game.mode) {
+                    case 0:
+                        ctx.fillText(cards[y * card.numX + x], card.width * (x + 0.5) + card.widthS * x, card.height * (y + 0.5) + card.heightS * y, card.width);
+                        break;
+                    case 1:
+                        ctx.fillStyle = getColorBW(cards[y * card.numX + x]);
+                        ctx.fillRect(card.width * x + card.widthS * x, card.height * y + card.heightS * y, card.width, card.height);
+                        ctx.fillStyle = 'black';
+                        break;
+                    }
+                }
+                
                 ctx.rect(card.width * x + card.widthS * x, card.height * y + card.heightS * y, card.width, card.height);
                 ctx.stroke();
-
-                if ((i == front.first) || (i == front.second)) {
-                    ctx.fillText(cards[y * card.numX + x], card.width * (x + 0.5) + card.widthS * x, card.height * (y + 0.5) + card.heightS * y, card.width);
-                }
             } else {
                 ctx.fillRect(card.width * x + card.widthS * x, card.height * y + card.heightS * y, card.width, card.height);
             }
@@ -202,4 +215,45 @@ function drawScore() {
     ctx.fillText('時間：' + (Math.floor(game.timer / 6000)) + '分' + (game.timer % 6000 / 100) + '秒', game.width / 2, card.height * 2, game.width);
     ctx.fillText('回数：' + game.count + '回', game.width / 2, card.height * 3, game.width);
     ctx.fillText('ミス：' + game.missCount + '回', game.width / 2, card.height * 4, game.width);
+}
+
+/* ボタンのデザインの変更 */
+function setButtonColors() {
+    resetButtonColor('startNum');
+    resetButtonColor('startBW');
+
+    switch(game.mode) {
+    case 0:
+        setButtonColorSelected('startNum');
+        break;
+    case 1:
+        setButtonColorSelected('startBW');
+        break;
+    }
+}
+
+/* 白黒モードの色の取得 */
+function getColorBW(num) {
+    switch (num) {
+    case 0:
+        return '#000000';
+    case 1:
+        return '#191919';
+    case 2:
+        return '#323232';
+    case 3:
+        return '#4B4B4B';
+    case 4:
+        return '#646464';
+    case 5:
+        return '#7D7D7D';
+    case 6:
+        return '#969696';
+    case 7:
+        return '#AFAFAF';
+    case 8:
+        return '#C8C8C8';
+    case 9:
+        return '#E1E1E1';
+    }
 }
