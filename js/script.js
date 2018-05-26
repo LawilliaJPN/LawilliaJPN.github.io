f5129df6794785cc1c263a2844e9718d458408f8
LawilliaJPN <lawillia.jp@gmail.com>
2018-05-26 09:34:53 +0900

    [clean]全体：共通html部分のJS化など

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// 共通ヘッダー
+$('#common-header').html('\
+<nav class="navbar navbar-expand-sm navbar-light">\
+	<a class="navbar-brand" href="https://lawilliajpn.github.io">\
+		<span class="mr-2">PCW</span>Web制作の練習\
+	</a>\
+\
+	<button class="navbar-toggler" data-toggle="collapse" data-target="#menu">\
+		<span class="navbar-toggler-icon"></span>\
+	</button>\
+	<div id="menu" class="collapse navbar-collapse">\
+		<ul class="navbar-nav">\
+			<li class="nav-item mr-1">\
+				<a class="nav-link" href="https://lawilliajpn.github.io/">TOP</a>\
+			</li>\
+			<li class="nav-item mr-1">\
+				<a class="nav-link" href="https://lawilliajpn.github.io/profile/">PROFILE</a>\
+			</li>\
+		</ul>\
+	</div>\
+</nav>\
+');
+
+// 共通フッター
+$('#common-footer').html('\
+<p class="text-right text-muted">\
+	<small>Copyright © 2017-2018 Lawillia All Rights Reserved.</small>\
+</p>\
+');
