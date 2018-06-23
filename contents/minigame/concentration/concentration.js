47d9dea7d4657e7189b50c89e0059a121e107b36
LawilliaJPN <lawillia.jp@gmail.com>
2018-06-24 02:15:19 +0900

    [clean]神経衰弱：ES6移行

--- contents/minigame/concentration/concentration.js
+++ contents/minigame/concentration/concentration.js
@@ -1,132 +1,150 @@
-var canvas, ctx;
-var game = {width:0, height:0, mode:0, isRunning:false, timer:0, count:0, missCount:0, pair:0};
-// カード描画関連
-var card = {width:0, height:0, widthS:0, heightS:0, numX:0, numY:0};
-// 各カードの中身
-var cards = [20];
-// 各カードが既にそろっているか
-var isPair = [20];
-// 既にその数字が出ているか
-var isAlreadyCard= [20];
-var isAlreadyNumber= [10];
-var isAlreadyNow = false;
-// 表のカード
-var front = {first:-1, second:-1};
+'use strict';
 
 /*
- * 入力
+ * グローバル変数
  */
-/* タイマー */
-setInterval(concentration, 10);
 
-/* ページを開いたとき */
-window.onload = function() {
-    updateCanvas();
-    start();
-}
+let canvas, ctx;
 
-/* ウィンドウのサイズが変わったとき */
-window.onresize = function() {
-    updateCanvas();
-    draw();
-}
+/* ゲーム全体 */
+const game = {
+    // Canvasのサイズ
+    width:0,
+    height:0,
+    // ゲームモード
+    mode:0,
+    // プレイ中か否か
+    isRunning:false,
+    // カードの枚数
+    numOfCards:20,
+    // ペア成立数
+    pair:0
+};
 
+/* スコア関連 */
+const result = {
+    timer:0,
+    count:0,
+    miss:0
+};
+
+/* カード描画関連 */
+const card = {
+    // カードのサイズ
+    width:0,
+    height:0,
+    // カード同士の隙間のサイズ
+    widthS:0,
+    heightS:0,
+    // カードが縦横何枚ずつ並ぶか
+    numX:0,
+    numY:0
+};
+
+/* 各カードの情報 */
+const cards = {
+    // 各カードの種類
+    number: [game.numOfCards],
+    // ペアが揃っているか否か
+    isPaired: [game.numOfCards],
+    // カードか既知の否か
+    isKnownCard: [game.numOfCards],
+    // 種類か既知の否か
+    isKnownNumber: [game.numOfCards / 2],
+    // 1枚目として選択中のカードが既知か否か
+    isKnownNow: false,
+    // 選択中のカード
+    selected: [-1, -1]
+};
+
+/*
+ * 基本処理
+ */
 /* ボタン */
-function btnStart(mode) {
+const btnStart = mode => {
     game.mode = mode;
     toggleButtonDisplay();
     start();
 }
 
-/* キーボード */
-document.onkeydown = function(e) {
-    start();
-}
-
-/*
- * 基本処理
- */
-function concentration() {
+const concentration = () => {
     if (!game.isRunning) return;
-    game.timer++;
+    result.timer++;
 }
 
-function start() {
+const start = () => {
     updateCanvas();
 
-    cards.length = 0;
-    for (var i = 0; i < 20; i++) {
-        cards[i] = i % 10;
+    cards.number.length = 0;
+    for (let i = 0; i < game.numOfCards; i++) {
+        cards.number[i] = i % 10;
+        cards.isPaired[i] = false;
+        cards.isKnownCard[i] = false;
+	cards.isKnownNumber[i / 2] = false;
     }
-    shuffleCards(cards, 100);
-
-    isPair = [false, false, false, false, false, false, false, false, false, false,
-            false, false, false, false, false, false, false, false, false, false];
-    isAlreadyCard = [false, false, false, false, false, false, false, false, false, false,
-            false, false, false, false, false, false, false, false, false, false];
-    isAlreadyNumber = [false, false, false, false, false, false, false, false, false, false];
+    shuffleCards(cards.number, 100);
 
     game.isRunning = false;
-    game.timer = 0;
-    game.count = game.missCount = 0;
-    game.pair = cards.length /2;
-    front.first = front.second = -1;
+    game.pair = cards.number.length /2;
+    result.timer = 0;
+    result.count = result.miss = 0;
+    cards.selected[0] = cards.selected[1] = -1;
 
     draw();
 }
 
-function clear() {
+const clear = () => {
     game.isRunning = false;
     draw();
 }
 
-function select(e) {
-    var rect = e.target.getBoundingClientRect();
-    var eventX = (e.clientX - Math.floor(rect.left)) % (card.width + card.widthS);
-    var eventY = (e.clientY - Math.floor(rect.top)) % (card.height + card.heightS);
-    var x = Math.floor((e.clientX - Math.floor(rect.left)) / (card.width + card.widthS));
-    var y = Math.floor((e.clientY - Math.floor(rect.top)) / (card.height + card.heightS));
-    var num = card.numX * y + x;
+const select = e => {
+    const rect = e.target.getBoundingClientRect();
+    const eventX = (e.clientX - Math.floor(rect.left)) % (card.width + card.widthS);
+    const eventY = (e.clientY - Math.floor(rect.top)) % (card.height + card.heightS);
+    const x = Math.floor((e.clientX - Math.floor(rect.left)) / (card.width + card.widthS));
+    const y = Math.floor((e.clientY - Math.floor(rect.top)) / (card.height + card.heightS));
+    const num = card.numX * y + x;
 
-    if ((eventX <= card.width) && (eventY <= card.height) && (isPair[num] == false)) {
-        if (front.first != -1) {
-            if (front.first == num) return;
+    if ((eventX <= card.width) && (eventY <= card.height) && (cards.isPaired[num] === false)) {
+        if (cards.selected[0] !== -1) {
+            if (cards.selected[0] === num) return;
             
-            front.second = num;
+            cards.selected[1] = num;
             draw();
 
             game.isRunning = true;
-            game.count++;
+            result.count++;
 
-            if (cards[front.first] == cards[front.second]) {
-                isPair[front.first] = true;
-                isPair[front.second] = true;
+            if (cards.number[cards.selected[0]] === cards.number[cards.selected[1]]) {
+                cards.isPaired[cards.selected[0]] = true;
+                cards.isPaired[cards.selected[1]] = true;
                 
                 game.pair--;
-                if (game.pair == 0) clear();
+                if (game.pair === 0) clear();
             } else {
-                if (isAlreadyNow) {
+                if (cards.isKnownNow) {
                     console.log('miss');
-                    game.missCount++;
-                } else if (isAlreadyCard[front.second]) {
+                    result.miss++;
+                } else if (cards.isKnownCard[cards.selected[1]]) {
                     console.log('miss');
-                    game.missCount++;
+                    result.miss++;
                 }
             }
 
-            isAlreadyNumber[cards[front.second]] = true;
-            isAlreadyCard[front.second] = true;
-            front.first = front.second = -1;
+            cards.isKnownNumber[cards.number[cards.selected[1]]] = true;
+            cards.isKnownCard[cards.selected[1]] = true;
+            cards.selected[0] = cards.selected[1] = -1;
         } else {
-            front.first = num;
+            cards.selected[0] = num;
             draw();
 
-            isAlreadyCard[front.first] = true;
-            if (isAlreadyNumber[cards[front.first]]) {
-                isAlreadyNow = true;
+            cards.isKnownCard[cards.selected[0]] = true;
+            if (cards.isKnownNumber[cards.number[cards.selected[0]]]) {
+                cards.isKnownNow = true;
             } else {
-                isAlreadyNumber[cards[front.first]] = true;
-                isAlreadyNow = false;
+                cards.isKnownNumber[cards.number[cards.selected[0]]] = true;
+                cards.isKnownNow = false;
             }
         }
     }
@@ -136,43 +154,43 @@ function select(e) {
  * 表示
  */
 /* Canvasの更新 */
-function updateCanvas() {
+const updateCanvas = () => {
     updateSize();
     getCanvas();
 }
 
 /* Canvasサイズの反映 */
-function updateSize() {
+const updateSize = () => {
     game.width = document.getElementById('game').clientWidth;
-    game.height = window.innerHeight /2;
+    game.height = window.innerHeight / 2;
 }
 
 /* Canvas要素を取得して行う処理 */
-function getCanvas() {
+const getCanvas = () => {
     updateHtml('game', '<canvas id="canvas" width="' + game.width + '" height="' + game.height + '">');
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
     /* ゲーム中 入力 */
-    canvas.addEventListener("mousedown",function(e){
+    canvas.addEventListener('mousedown',function(e){
         select(e);
     });
 }
 
-function draw() {
-    if (game.pair == 0) drawScore();
+const draw = () => {
+    if (game.pair === 0) drawScore();
     else drawGame();
 }
 
 /* ゲームの描画 */
-function drawGame() {
+const drawGame = () => {
     // 画面をクリア
     ctx.clearRect(0, 0, game.width, game.height);
 
     // カード描画関連の初期化
     card.num = 20;
     card.numX = 5;
-    card.numY = cards.length / card.numX;
+    card.numY = cards.number.length / card.numX;
     card.width = game.width / (card.numX + 1);
     card.height = game.height / (card.numY + 1);
     card.widthS = card.width / (card.numX - 1);
@@ -184,17 +202,17 @@ function drawGame() {
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
 
-    var i = 0;
-    for (var y = 0; y < card.numY; y++){
-        for (var x = 0; x < card.numX; x++){
-            if (!isPair[i]) {
-                if ((i == front.first) || (i == front.second)) {
+    let i = 0;
+    for (let y = 0; y < card.numY; y++){
+        for (let x = 0; x < card.numX; x++){
+            if (!cards.isPaired[i]) {
+                if ((i === cards.selected[0]) || (i === cards.selected[1])) {
                     switch (game.mode) {
                     case 0:
-                        ctx.fillText(cards[y * card.numX + x], card.width * (x + 0.5) + card.widthS * x, card.height * (y + 0.5) + card.heightS * y, card.width);
+                        ctx.fillText(cards.number[y * card.numX + x], card.width * (x + 0.5) + card.widthS * x, card.height * (y + 0.5) + card.heightS * y, card.width);
                         break;
                     case 1:
-                        ctx.fillStyle = getColorBW(cards[y * card.numX + x]);
+                        ctx.fillStyle = getColorBW(cards.number[y * card.numX + x]);
                         ctx.fillRect(card.width * x + card.widthS * x, card.height * y + card.heightS * y, card.width, card.height);
                         ctx.fillStyle = 'black';
                         break;
@@ -212,7 +230,7 @@ function drawGame() {
 }
 
 /* ゲームクリア画面の描画 */
-function drawScore() {
+const drawScore = () => {
     // 画面をクリア
     ctx.clearRect(0, 0, game.width, game.height);
 
@@ -223,15 +241,15 @@ function drawScore() {
     ctx.textBaseline = 'top';
 
     ctx.fillText('CLEAR', game.width / 2, card.height * 0, game.width);
-    ctx.fillText('時間：' + (Math.floor(game.timer / 6000)) + '分' + (game.timer % 6000 / 100) + '秒', game.width / 2, card.height * 1.5, game.width);
-    ctx.fillText('回数：' + game.count + '回', game.width / 2, card.height * 2.5, game.width);
-    ctx.fillText('ミス：' + game.missCount + '回', game.width / 2, card.height * 3.5, game.width);
+    ctx.fillText(`時間：${Math.floor(result.timer / 6000)}分${(result.timer % 6000 / 100)}秒`, game.width / 2, card.height * 1.5, game.width);
+    ctx.fillText(`回数：${result.count}回`, game.width / 2, card.height * 2.5, game.width);
+    ctx.fillText(`ミス：${result.miss}回`, game.width / 2, card.height * 3.5, game.width);
 }
 
 /* ボタンの見た目の切り替え */
-function toggleButtonDisplay() {
-    var startNum = document.getElementById('startNum');
-    var startBW = document.getElementById('startBW');
+const toggleButtonDisplay = () => {
+    const startNum = document.getElementById('startNum');
+    const startBW = document.getElementById('startBW');
 
     switch(game.mode) {
     case 0:
@@ -260,7 +278,7 @@ function toggleButtonDisplay() {
 }
 
 /* 白黒モードの色の取得 */
-function getColorBW(num) {
+const getColorBW = num => {
     switch (num) {
     case 0:
         return '#000000';
@@ -284,3 +302,26 @@ function getColorBW(num) {
         return '#E1E1E1';
     }
 }
+
+/*
+ * 入力
+ */
+
+/* ページを開いたとき */
+window.onload = function() {
+    updateCanvas();
+    start();
+}
+
+/* ウィンドウのサイズが変わったとき */
+window.onresize = function() {
+    updateCanvas();
+    draw();
+}
+
+/* キーボード */
+document.onkeydown = function(e) {
+    start();
+}
+/* タイマー */
+setInterval(concentration, 10);
